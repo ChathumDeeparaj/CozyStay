38fb93248d08c145eb1c1efe7869d65cbb90064e
Chathum Deeparaj Rajamanthri <164840623+ChathumDeeparaj@users.noreply.github.com>
2025-06-06 13:22:09 +0530

    add file to repo

---
+++ CozyStay/src/main/java/com/example/CozyStay/CozyStayApplication.java
@@ -0,0 +1,13 @@
+package com.example.CozyStay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CozyStayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CozyStayApplication.class, args);
+	}
+
+}
